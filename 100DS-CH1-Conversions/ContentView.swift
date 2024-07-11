5fc572689f5111d4301cac4de8627713cff01781
Erica Sampson <ericabcherry@gmail.com>
2024-07-11 10:58:13 -0400

    Initial Commit

---
+++ 100DS-CH1-Conversions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DS-CH1-Conversions
+//
+//  Created by Erica Sampson on 2024-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DS-CH1-Conversions/_00DS_CH1_ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_CH1_ConversionsApp.swift
+//  100DS-CH1-Conversions
+//
+//  Created by Erica Sampson on 2024-07-11.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_CH1_ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
